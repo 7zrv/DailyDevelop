d0854671524feb18544246b1d243ed5c6f42d018
7zrv <4014133@gmail.com>
2024-09-02 10:54:33 +0900

    Initial commit

---
+++ src/main/java/com/yoon/dailydevelop/DailyDevelopApplication.java
@@ -0,0 +1,16 @@
+package com.yoon.dailydevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing //등록시각, 수정시각을 위한 전체 auditing 활성화를 위한 에노테이션
+@SpringBootApplication
+public class DailyDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoon/dailydevelop/DailyDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.dailydevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
